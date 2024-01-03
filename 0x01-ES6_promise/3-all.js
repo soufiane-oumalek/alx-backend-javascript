2e356f1812a0fcf67d7df9303a951793f42183ee
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-03 21:41:30 +0100

    ES6_Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((res) => {
+    .then((result) => {
       console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
