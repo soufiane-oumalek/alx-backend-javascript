93ae13704710749525d3b2d3f9f6111390e758ae
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-03 21:36:27 +0100

    ES6_Promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,6 +3,7 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
-    .finally(() => { console.log('Got a response from the API');
+    .finally(() => {
+      console.log('Got a response from the API');
     });
 }
