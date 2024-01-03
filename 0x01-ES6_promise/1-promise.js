92338bf191510bc80daf6b1aab1acf635d9581b1
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-03 21:25:30 +0100

    ES6_Promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200,
-      body: "success"
-    });
-  } else {
-    reject(new Error("the fake API is not working currently"));
-  }
-});
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
