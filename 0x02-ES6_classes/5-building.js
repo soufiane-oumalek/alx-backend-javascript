a6b1e931d75ca63925c167f63fa1f465263e34fa
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-05 00:58:56 +0100

    classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+// define HolbertonCourse class
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(valu) {
+    this._sqft = valu;
+  }
+}
