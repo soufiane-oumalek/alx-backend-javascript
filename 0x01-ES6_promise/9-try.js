9659f21d41f92725249863bc35f48740b2eb30be
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-04 01:27:36 +0100

    ES6_Promises

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,12 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-
   try {
-    queue.push(mathFunction());
-  } catch (err) {
-    queue.push(err.toString());
+    const res = mathFunction();
+    queue.push(res);
+  } catch (error) {
+    queue.push(error.toString());
   } finally {
     queue.push('Guardrail was processed');
   }
-
   return queue;
 }
