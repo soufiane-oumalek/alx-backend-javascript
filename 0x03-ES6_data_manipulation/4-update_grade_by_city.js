b2d7a82cc0767c29533bc8aac0ca868969a46c36
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-09 14:36:19 +0100

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 export default function updateStudentGradeByCity(listStudents, city, newGrades) {
   return listStudents.filter((student) => student.location === city).map((student) => {
-    const gradeObj = newGrades.find((grade) => student.id === grade.studentId);
-    const grade = gradeObj ? gradeObj.grade : 'N/A';
+    const gradeobject = newGrades.find((grade) => student.id === grade.studentId);
+    const grade = gradeobject ? gradeobject.grade : 'N/A';
     return { ...student, grade };
   });
 }
