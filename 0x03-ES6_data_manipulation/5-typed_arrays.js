380a707f6a2470779cd44f040cb50cb9a54e7a40
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-10 00:30:31 +0100

    typed_arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,14 @@
+function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+
+  int8View[position] = value;
+
+  return new DataView(buffer);
+}
+
+export default createInt8TypedArray;
